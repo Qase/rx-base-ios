b2c7ff7000b811d36321476992805a18123dde64
Martin Troup <martin.troup@quanti.cz>
2019-02-04 13:01:52 +0100

    FEAT: Initial project setup.

---
+++ QuantiRxBase/QuantiRxBase/SupportingFiles/QuantiRxBase.h
@@ -0,0 +1,19 @@
+//
+//  QuantiRxBase.h
+//  QuantiRxBase
+//
+//  Created by Martin Troup on 04/02/2019.
+//  Copyright © 2019 quanti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuantiRxBase.
+FOUNDATION_EXPORT double QuantiRxBaseVersionNumber;
+
+//! Project version string for QuantiRxBase.
+FOUNDATION_EXPORT const unsigned char QuantiRxBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuantiRxBase/PublicHeader.h>
+
+
